efd95b4314e8888c6ff49586f0063d8f59e35927
vagrant <vagrant@precise64.(none)>
2013-05-03 13:11:21 +0000

    0_O

--- app/models/persone.rb
+++ app/models/persone.rb
@@ -5,14 +5,13 @@ class Persone < ActiveRecord::Base
   # pg_search_scope :search_by_full_name, 
   # 	:against => [:first_name, :last_name],
   # 	:ignoring => :accents,
-  # 	:using => {:tsearch => {:dictionary => "english"}}
+  # 	:using => {}
 
   # pg_search_scope :search_name, :against => :first_name, :using => [:tsearch, :trigram, :dmetaphone]
 
   pg_search_scope :that_sounds_like,
-                  :against => [:first_name, :last_name],
-                  :using => [:trigram, :dmetaphone]
-
+									:against => [:first_name, :last_name],
+									:using => [:trigram, :dmetaphone]
 
   def self.search(query)
     if query.present?
